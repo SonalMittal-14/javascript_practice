304193711ac5919bc5a3d8fc7f6a65190af9f840
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-01-23 23:35:53 +0530

    gh

--- objects.js
+++ objects.js
@@ -37,5 +37,39 @@ console.log(JsUser.greetingTwo());
 
 // const tinderUser = new Object()  // singleton object
 const tinderUser = {} //non singleton object
+tinderUser.id = "123abc"
+tinderUser.name = "sonal"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "hello@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname : "sonal",
+            lastname : "mittal"
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1 : "a" , 2 : "b"}
+const obj2 = {3 : "c" , 4 : "d"}
+const obj4 = {5 : "c" , 6 : "d"}
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj3 = {...obj1, ...obj2, ...obj4}
+console.log(obj3);
+
+const users = {
+    id : 1,
+    email : "fghj@gmail.com"
+}
 
 console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedin'));
