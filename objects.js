afc2d6ad2d7cc660133317ce2b402a1fddcfeb47
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-02-01 19:34:49 +0530

    cvbn

--- objects.js
+++ objects.js
@@ -70,6 +70,6 @@ const users = {
 console.log(tinderUser);
 console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLoggedin'));
