2429284784c26f8a8810f5f7ea069615f4411d60
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-02-11 23:04:23 +0530

    vbn

--- objects.js
+++ objects.js
@@ -70,7 +70,7 @@ const users = {
 console.log(tinderUser);
 console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLoggedin'));
 
