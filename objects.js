fdaaf13bf71abb86b3e788d06bc87b698295b7de
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-02-18 22:59:56 +0530

    dgdgg

--- objects.js
+++ objects.js
@@ -59,7 +59,7 @@ console.log(regularUser.fullname.userfullname);
 const obj1 = {1 : "a" , 2 : "b"}
 const obj2 = {3 : "c" , 4 : "d"}
 const obj4 = {5 : "c" , 6 : "d"}
-const obj3 = {obj1, obj2}
+// const obj3 = {obj1, obj2}
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 const obj3 = {...obj1, ...obj2, ...obj4}
 console.log(obj3);
