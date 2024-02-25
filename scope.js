c9ee972715b88746490ae7f045028b3da1216652
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-02-25 22:26:56 +0530

    fgh

--- scope.js
+++ scope.js
@@ -12,3 +12,38 @@ console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one() {
+    const username = "sonal"
+
+    function two() { //two one ke sab cheze acess kar sakta h 
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website); //ye nahi chalega kyuli ye two() ka part h
+
+    two()
+}
+one()
+
+
+if(true){
+    const username = "sonal"
+    if(username === "sonal") {
+        const website = " goggle"
+        console.log(username + website);
+    }
+}
+
+
+// +++++++ interesting ++++++++
+
+
+function addone(num){
+    return num+1
+}
+addone(5)
+
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(5)
